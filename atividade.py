b8be4b12ac8dc9c20999e307e5671777b86b2354
fernanda <feh_strebe@hotmail.com>
2018-10-05 13:35:44 -0300

    tutorial django, terceiro commit

--- atividade.py
+++ atividade.py
@@ -1,5 +1,56 @@
 #print("Hello, Django Girls!")
-if 3 > 2:
-    print("3 é maior que dois")
-else:
-    print("3 não ")
+#if 3 > 2:
+ #   print("3 é maior que dois")
+#else:
+#    print("3 não ")
+
+#nome = "fernanda"
+#if nome == "anjo":
+#    print("oi anjo")
+#elif nome == "fernanda":
+#    print("oi fernanda")
+#else:
+#    print("oi estranho")
+
+#volume = 67
+#if volume < 20:
+#    print("nao ouço a musica")
+#elif volume <= 30:
+#    print("está quase bom para musica ambiente")
+#elif volume <= 40:
+#    print("esta em volume ambiente")
+#elif volume <= 50:
+#    print("esta um pouco alto")
+#else:
+#    print("estou surdo")
+
+#volume = 40
+#if volume > 20 or volume < 60:
+#    print("muito bom!")
+
+#def oi():
+#    print("oi!")
+#    print("tudo bem?")
+
+#oi()
+
+#def oi(nome):
+#    if nome == "olá":
+#        print("oi")
+#    elif nome == "fernanda":
+#        print("oi fernanda")
+#    else:
+#        print("oi estranho")
+
+#oi("")
+
+#def oi(nome):
+#    print("Olá " + nome + "!")
+
+#girls = ["fernanda", "joana", "paula", "ana"]
+#for nome in girls:
+#    oi(nome)
+#    print("Próxima")
+
+for i in range(1, 6):
+    print (i)
